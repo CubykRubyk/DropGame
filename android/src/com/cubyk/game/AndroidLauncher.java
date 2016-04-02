f38025d5bf5d4304720acb0df0d43db05587b724
CubykRubyk <ion.damaschin@hotmail.com>
2016-04-02 12:37:06 +0300

    Update DropGame

--- android/src/com/cubyk/game/AndroidLauncher.java
+++ android/src/com/cubyk/game/AndroidLauncher.java
@@ -4,13 +4,14 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.cubyk.game.MyGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MyGame(), config);
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		initialize(new DropGame(), config);
 	}
 }
--- desktop/src/com/cubyk/game/desktop/DesktopLauncher.java
+++ desktop/src/com/cubyk/game/desktop/DesktopLauncher.java
@@ -2,11 +2,16 @@ package com.cubyk.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.cubyk.game.MyGame;
+import com.cubyk.game.DropGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGame(), config);
+        config.title = "Drop";
+        config.width = 800;
+        config.height = 480;
+		new LwjglApplication(new DropGame(), config);
 	}
 }
+
+
