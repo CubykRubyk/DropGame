f5686515d8575f76d9840a232a46c804b9e0d86a
Ion Damaschin <Ion Damaschin>
2016-04-02 10:34:47 +0300

    LibGDX first project

---
+++ desktop/src/com/cubyk/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cubyk.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cubyk.game.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
